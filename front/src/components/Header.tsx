2818d75b84c815bf45fa9b896e2e171ae0ae10d0
Danil <d_silchenkov@mail.ru>
2026-04-13 23:45:45 +0200

    save

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -8,40 +8,57 @@ import { useUser } from "@/hooks/useUser";
 import { useState } from "react";
 
 export default function Header() {
-    const user = JSON.parse(localStorage.getItem('user') || '{}');
+    const user = JSON.parse(localStorage.getItem("user") || "{}");
     const { isAdmin } = useUser();
     const [isLogoutDialogOpen, setIsLogoutDialogOpen] = useState(false);
     return (
         <section className="container flex flex-wrap justify-between lg:border-none border-b py-4! sm:mb-0! mb-4!">
-            <Link to='/profile'>
-                {user.role === 'admin' ? 'Администратор' :
-                    user.role === 'storekeeper' ? 'Работник склада' :
-                        user.role === 'accountant' ? 'Бухгалтер' :
-                            'Неизвестная роль'}
+            <Link to="/profile">{user.role === "admin" ? "Администратор" : user.role === "storekeeper" ? "Работник склада" : user.role === "accountant" ? "Бухгалтер" : "Неизвестная роль"}</Link>
+            <Link to="/profile" className="lg:flex hidden">
+                {user.name + " " + user.secondname}
             </Link>
-            <Link to='/profile' className="lg:flex hidden">{user.name + ' ' + user.secondname}</Link>
 
             <div className="items-center flex gap-2 ">
                 <DarkModeButtonToggle />
 
-
                 <DropdownMenu>
-                    <DropdownMenuTrigger><Button variant='outline' className="px-3"><img src="/setting.png" alt="" className="w-5 icon" /></Button></DropdownMenuTrigger>
+                    <DropdownMenuTrigger>
+                        <Button variant="outline" className="px-3">
+                            <img src="/setting.png" alt="" className="w-5 icon" />
+                        </Button>
+                    </DropdownMenuTrigger>
                     <DropdownMenuContent>
                         <DropdownMenuLabel>Инструменты</DropdownMenuLabel>
                         <DropdownMenuSeparator />
                         {isAdmin && (
-                            <DropdownMenuItem><Link to='/backups'>Бэкапы</Link></DropdownMenuItem>
+                            <DropdownMenuItem>
+                                <Link to="/backups">Бэкапы</Link>
+                            </DropdownMenuItem>
                         )}
-                        <DropdownMenuItem><Link to='/profile'>Профиль</Link></DropdownMenuItem>
-                        <DropdownMenuItem className="lg:hidden"><Link to='/allusers'>Все пользователи</Link></DropdownMenuItem>
-                        <DropdownMenuItem><Link to='/inventories'>Инвентаризация</Link></DropdownMenuItem>
-                        <DropdownMenuItem><Link to='/dashboard'>Дашборд</Link></DropdownMenuItem>
-                        <DropdownMenuItem><Link to='/reports'>Отчеты</Link></DropdownMenuItem>
-                        <DropdownMenuItem className="cursor-pointer" onSelect={(e) => {
-                            e.preventDefault();
-                            setIsLogoutDialogOpen(true);
-                        }}>
+                        <DropdownMenuItem>
+                            <Link to="/profile">Профиль</Link>
+                        </DropdownMenuItem>
+                        {isAdmin && (
+                            <DropdownMenuItem className="lg:hidden">
+                                <Link to="/allusers">Все пользователи</Link>
+                            </DropdownMenuItem>
+                        )}
+                        <DropdownMenuItem>
+                            <Link to="/inventories">Инвентаризация</Link>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                            <Link to="/dashboard">Дашборд</Link>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                            <Link to="/reports">Отчеты</Link>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem
+                            className="cursor-pointer"
+                            onSelect={(e) => {
+                                e.preventDefault();
+                                setIsLogoutDialogOpen(true);
+                            }}
+                        >
                             Выйти
                         </DropdownMenuItem>
                     </DropdownMenuContent>
@@ -54,15 +71,19 @@ export default function Header() {
                         </AlertDialogHeader>
                         <AlertDialogFooter>
                             <AlertDialogCancel>Отмена</AlertDialogCancel>
-                            <AlertDialogAction onClick={() => {
-                                localStorage.removeItem('token');
-                                localStorage.removeItem('user');
-                                window.location.href = '/login';
-                            }}>Выйти</AlertDialogAction>
+                            <AlertDialogAction
+                                onClick={() => {
+                                    localStorage.removeItem("token");
+                                    localStorage.removeItem("user");
+                                    window.location.href = "/login";
+                                }}
+                            >
+                                Выйти
+                            </AlertDialogAction>
                         </AlertDialogFooter>
                     </AlertDialogContent>
                 </AlertDialog>
             </div>
         </section>
-    )
+    );
 }
