a4377ea71aa77c47d180d9adf92e94325f8b1d27
Danil <d_silchenkov@mail.ru>
2026-05-11 07:12:15 +0300

    save

--- front/src/services/api.ts
+++ front/src/services/api.ts
@@ -1,22 +1,22 @@
 import axios from "axios";
 
 //vps
-// const getApiBaseUrl = () => {
-//     if (import.meta.env.VITE_API_URL) {
-//         return import.meta.env.VITE_API_URL;
-//     }
-//     return "/api";
-// };
+const getApiBaseUrl = () => {
+    if (import.meta.env.VITE_API_URL) {
+        return import.meta.env.VITE_API_URL;
+    }
+    return "/api";
+};
 
 //local
 
-const getApiBaseUrl = () => {
-    if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") {
-        return "http://localhost:3000";
-    }
-    // return import.meta.env.VITE_API_URL || "https://бэкенд.onrender.com";
-    return `http://${window.location.hostname}:3000`;
-};
+// const getApiBaseUrl = () => {
+//     if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") {
+//         return "http://localhost:3000";
+//     }
+//     // return import.meta.env.VITE_API_URL || "https://бэкенд.onrender.com";
+//     return `http://${window.location.hostname}:3000`;
+// };
 
 export const API_BASE_URL = getApiBaseUrl();
 
